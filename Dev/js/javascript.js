796966c683b40049ea366f2ee4cdb9ca5e869ad1
Jesus Coloma <colomacardoza16@gmail.com>
2022-11-27 14:17:23 -0500

    Modificación de slide con 4 imaganes y se coloco el nav dentro del header

--- Dev/js/javascript.js
+++ Dev/js/javascript.js
@@ -1,12 +1,15 @@
 
 window.addEventListener("scroll",function(){
-    let header=document.querySelector(".main-header");
-    header.classList.toggle("main-header--blanco",window.scrollY>0);
+    let header=document.querySelector(".main-header__container");
+    header.classList.toggle("main-header__container--blanco",window.scrollY>0);
 
     let nav=document.querySelector(".main-nav");
-    nav.classList.toggle("main-nav--azul",window.scrollY>0);
-
+    nav.classList.toggle("main-nav--blue",window.scrollY>0);
+    
     let titleh=document.querySelector(".main-header__title");
     titleh.classList.toggle("main-header__title--morado",window.scrollY>0);
+
+    let buttonG=document.querySelector(".button--email");
+    buttonG.classList.toggle("button--blue",window.scrollY>0);
 
 });
