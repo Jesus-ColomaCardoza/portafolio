6d381488adc6593a8963147e2104593937da85f7
Jesus Coloma <colomacardoza16@gmail.com>
2022-12-08 23:10:59 -0500

    se agrego un menu responsive: main-menu--mobile y main-menu--desktop

--- Dev/js/javascript.js
+++ Dev/js/javascript.js
@@ -3,8 +3,14 @@ window.addEventListener("scroll",function(){
     let header=document.querySelector(".main-header__container");
     header.classList.toggle("main-header__container--blanco",window.scrollY>0);
 
-    let nav=document.querySelector(".main-nav");
-    nav.classList.toggle("main-nav--blue",window.scrollY>0);
+    let nav=document.querySelectorAll(".main-nav");
+    let arrayNav=Array.from(nav);
+    arrayNav.map((a)=>{
+        a.classList.toggle("main-nav--blue",window.scrollY>0);
+    });
+
+    let menu=document.querySelector(".main-nav--mobile .main-menu");
+    menu.classList.toggle("main-menu--blue",window.scrollY>0);
     
     let titleh=document.querySelector(".main-header__title");
     titleh.classList.toggle("main-header__title--morado",window.scrollY>0);
