ccf621b9ff1bd9edb0ce0d80e4b38abef62cd2b6
Jesus Coloma <colomacardoza16@gmail.com>
2023-03-25 22:50:47 -0500

    Main menu responsive finished

--- Dev/js/javascript.js
+++ Dev/js/javascript.js
@@ -1,37 +1,33 @@
-//header
-window.addEventListener("scroll",function(){
-    let header=document.querySelector(".main-header__container");
-    header.classList.toggle("main-header__container--blanco",window.scrollY>0);
-
-    let nav=document.querySelectorAll(".main-nav");
-    let arrayNav=Array.from(nav);
-    arrayNav.map((a)=>{
-        a.classList.toggle("main-nav--black",window.scrollY>0);
-    });
-  
-    let titleh=document.querySelector(".main-header__title");
-    titleh.classList.toggle("main-header__title--black",window.scrollY>0);
-
-    let buttonG=document.querySelector(".button--email");
-    buttonG.classList.toggle("button--black",window.scrollY>0);
-
-});
-
-//preloader
+const header=document.querySelector(".main-header__container");
+const titleh=document.querySelector(".main-header__title");
+const buttonG=document.querySelector(".button--email");
+const mainNav=document.getElementById('main-nav');
+const mainMenuNav=document.getElementById('main-menu--nav')
 const preloaderContainer=document.getElementById('preloader__container');
 const body=document.getElementById('body');
+
+//preloader
 setTimeout(()=>{
-    preloaderContainer.classList.add('close')
+    preloaderContainer.classList.add('hide')
     body.classList.add('show-overflowy')
 },3000);
 
+//header
+window.addEventListener("scroll",function(){
+    header.classList.toggle("main-header__container--blanco",window.scrollY>0);
+
+    titleh.classList.toggle("main-header__title--black",window.scrollY>0);
+
+    buttonG.classList.toggle("button--black",window.scrollY>0);
+    
+    mainNav.classList.toggle("main-nav--black",window.scrollY>0);
+
+    mainMenuNav.classList.toggle("main-menu--black",window.scrollY>0);
+});
 
 //menu
-const mainNav=document.getElementById('main-nav');
-const mainMenuNav=document.getElementById('main-menu--nav')
 mainNav.addEventListener('click',(e)=>{
-    if (e.target.classList.contains('button-bars') ||e.target.classList.contains('main-menu__text')) {
+    if (e.target.classList.contains('button-bars') ||e.target.classList.contains('main-menu__text') || e.target.classList.contains('main-menu__icon')) {
         mainMenuNav.classList.toggle('main-menu--show')
     }
-    
 })
