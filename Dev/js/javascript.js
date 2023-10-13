7e99b4556a603f94236f1d5da9c3174bc5718698
Jesus Coloma <colomacardoza16@gmail.com>
2023-10-13 15:48:58 -0500

    I added a css effect of typing to the start text

--- Dev/js/javascript.js
+++ Dev/js/javascript.js
@@ -1,16 +1,26 @@
-const header = document.querySelector(".main-header__container");
-const titleh = document.querySelector(".main-header__title");
+const header = document.querySelector('.main-header__container');
+const titleh = document.querySelector('.main-header__title');
 const buttonG = document.querySelector(".button--cv");
 const mainNav = document.getElementById('main-nav');
 const mainMenuNav = document.getElementById('main-menu--nav')
 const preloaderContainer = document.getElementById('preloader__container');
 const body = document.getElementById('body');
+const typing12= document.getElementById('typing1');
+const typing13= document.getElementById('typing2');
 
 //preloader
 setTimeout(() => {
     preloaderContainer.classList.add('hide')
     body.classList.add('show-overflowy')
 }, 2000);
+
+//animation typing
+setTimeout(()=>{    
+    typing12.classList.add('typing--12');
+},2000);
+setTimeout(()=>{    
+    typing13.classList.add('typing--13');
+},8000);
 
 //header
 window.addEventListener("scroll", () => {
