79f1bf7a93c6bac4d7f679dc63f86ad14e3127b6
Jesus Coloma <colomacardoza16@gmail.com>
2023-12-02 21:23:49 -0500

    I added the design of thanks page after of coments the contact form

--- Dev/js/javascript.js
+++ Dev/js/javascript.js
@@ -5,8 +5,21 @@ const mainNav = document.getElementById('main-nav');
 const mainMenuNav = document.getElementById('main-menu--nav')
 const preloaderContainer = document.getElementById('preloader__container');
 const body = document.getElementById('body');
-const typing12= document.getElementById('typing1');
-const typing13= document.getElementById('typing2');
+const typing12 = document.getElementById('typing1');
+const typing13 = document.getElementById('typing2');
+
+
+const changeColorHeader = (condition) => {
+    header.classList.toggle("main-header__container--blanco", condition);
+
+    titleh.classList.toggle("main-header__title--black", condition);
+
+    buttonG.classList.toggle("button--black", condition);
+
+    mainNav.classList.toggle("main-nav--black", condition);
+
+    mainMenuNav.classList.toggle("main-menu--black", condition);
+}
 
 //preloader
 setTimeout(() => {
@@ -15,32 +28,30 @@ setTimeout(() => {
 }, 2000);
 
 //animation typing
-setTimeout(()=>{    
+setTimeout(() => {
     typing12.classList.add('typing--12');
-},2000);
-setTimeout(()=>{    
+}, 2000);
+setTimeout(() => {
     typing13.classList.add('typing--13');
-},8000);
+}, 8000);
 
 //header
 window.addEventListener("scroll", () => {
-    header.classList.toggle("main-header__container--blanco", window.scrollY > 0);
-
-    titleh.classList.toggle("main-header__title--black", window.scrollY > 0);
-
-    buttonG.classList.toggle("button--black", window.scrollY > 0);
-
-    mainNav.classList.toggle("main-nav--black", window.scrollY > 0);
-
-    mainMenuNav.classList.toggle("main-menu--black", window.scrollY > 0);
+    changeColorHeader(window.scrollY > 0);
 });
 
 //menu
 mainNav.addEventListener('click', (e) => {
     if (e.target.classList.contains('button-bars') ||
         e.target.classList.contains('main-menu__text') ||
-        e.target.classList.contains('main-menu__icon')) {
+        e.target.classList.contains('main-menu__icon') ||
+        e.target.classList.contains('main-menu__link')) {
+
 
         mainMenuNav.classList.toggle('main-menu--show')
+
+        if (window.scrollY == 0) {
+            changeColorHeader()
+        }
     }
 })
---
+++ Dev/js/thanks.js
@@ -0,0 +1,3 @@
+setTimeout(()=>{
+    window.location.replace("index.html");
+},5000);
